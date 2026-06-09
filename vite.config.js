15b2786990fff040a64c5bb339209726407a898a
风刀客 <16567025+fengdaoke@user.noreply.gitee.com>
2026-06-09 11:48:00 +0800

    修复已知问题

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 //import { alphaTab } from '@coderline/alphatab-vite';
 
 export default defineConfig({
-  base: './',  // 相对路径
+  base: '/midi-PianoFlow/',  // 相对路径
   build: {
     outDir: 'docs'  // 打包到 docs 文件夹
   }
